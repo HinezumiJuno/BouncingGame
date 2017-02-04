ac75235be19b31ee0d721e16df74d82994e2dc08
HinezumiJuno <8Juno.Minazuki8@gmail.com>
2017-02-05 00:26:06 +0900

    Add the paddle sprite.

--- BouncingGame/GameLayer.cs
+++ BouncingGame/GameLayer.cs
@@ -4,8 +4,14 @@ using CocosSharp;
 namespace BouncingGame{
 	public class GameLayer : CCLayerColor
 	{
+		CCSprite paddleSprite;
 		public GameLayer() : base (CCColor4B.Black)
 		{
+			// "paddle" refers to the paddle.png image
+			paddleSprite = new CCSprite("paddle");
+			paddleSprite.PositionX = 100;
+			paddleSprite.PositionY = 100;
+			AddChild(paddleSprite);
 		}
 
 		void RunGameLogic(float frameTimeInSeconds)
@@ -14,10 +20,22 @@ namespace BouncingGame{
 
 		protected override void AddedToScene()
 		{
+			base.AddedToScene();
+			// Use the bounds to layout the positioning of our drawable assets
+			CCRect bounds = VisibleBoundsWorldspace;
+			// Register for touch events
+			var touchListener = new CCEventListenerTouchAllAtOnce();
+			touchListener.OnTouchesEnded = OnTouchesEnded;
+			AddEventListener(touchListener, this);
+
 		}
 
 		void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
 		{
+			if (touches.Count > 0)
+			{
+				// Perform touch handling here
+			}
 		}
 
 		void HandleTouchesMoved(List<CCTouch> touches, CCEvent touchEvent)
